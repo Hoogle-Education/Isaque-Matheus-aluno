47cf0c9f4d1a263c7a388027b2fb7d60dba9c3b8
Hugo Rafael <hugorafell@gmail.com>
2022-04-10 19:20:01 -0300

    Update aula 09/04/22

--- Films Project/ReadFilmData.java
+++ Films Project/ReadFilmData.java
@@ -1,56 +1,107 @@
 import java.io.File;
 import java.util.*;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class ReadFilmData {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) {
 		//parsing and reading the CSV file data into the film (object) array
 		// provide the path here...
-    File directory = new File("C:\\Users\\hugor\\Desktop\\Aulas\\alunos\\Isaque\\Films Project\\Film.csv");
-  	// String name = directory.getAbsolutePath() + "//Film.csv";
-		Scanner sc = new Scanner(directory);
-		Film[] films = new Film[10000];
+    File directory = new File("");
+		System.out.println(directory.getAbsolutePath());
+		String path = directory.getAbsolutePath() + "\\Film.csv";
 
-		// this will just print the header in CSV file
-		sc.nextLine();
-
-		int i = 0; String st = "";
+		Scanner sc = null;
+		Film[] films = null; 
 		SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd");
-		while (sc.hasNextLine())  //returns a boolean value
-		{
-			st = sc.nextLine();
-			String[] data = st.split(",");
-			films[i++] = new Film(Integer.parseInt(data[0]), data[3], data[1], ft.parse(data[2]), Float.parseFloat(data[4]), Float.parseFloat(data[5]));
-		}
-		sc.close();  //closes the scanner
 
-		// We can print film details due to overridden toString method in film class
-    
-    // time to sort
-    timeToSort(films, 10);
-    timeToSort(films, 100);
-    timeToSort(films, 1000);
-    timeToSort(films, 5000);
-    timeToSort(films, 10000);
+  	// String name = directory.getAbsolutePath() + "//Film.csv";
+		try {
+
+			sc = new Scanner(path);
+			films = new Film[10000];
+			
+			// this will just print the header in CSV file
+			sc.nextLine();
+			
+			int size = 0; String st = "";
+
+			while (sc.hasNextLine())  //returns a boolean value
+			{
+				try {
+					st = sc.nextLine();
+					String[] data = st.split(",");
+
+					Date release = ft.parse(data[2]);
+					float filmLength = Float.parseFloat(data[4]);
+
+					if( release.before( ft.parse("1895-12-01")))
+						throw new 
+							InputMismatchException("Film date older than film industry");
+					else if( release.after( ft.parse("2022-04-11")) )
+						throw new 
+							InputMismatchException("Film date is in the future");
+					else if( filmLength > 3.2 )
+						throw new
+							InputMismatchException("Film is too long");
+					else if ( filmLength < 0.0 )
+						throw new
+							InputMismatchException("length = " 
+																			+ filmLength 
+																			+ " is not valid" );
+
+					films[size++] = new Film(Integer.parseInt(data[0]), data[3], data[1], release , filmLength , Float.parseFloat(data[5]));
+				} catch(InputMismatchException imex){
+					System.out.println(imex.getMessage());
+					imex.printStackTrace();
+				}
+
+			}
+			// time to sort
+			// We can print film details due to overridden toString method in film class
+			timeToSort(films, 10);
+			timeToSort(films, 100);
+			timeToSort(films, 1000);
+			timeToSort(films, 5000);
+			timeToSort(films, 10000);
+
+		} catch( RuntimeException rex ){
+			System.out.println(rex.getMessage());
+			rex.printStackTrace();
+		} catch( ParseException pex ){
+			System.out.println(pex.getMessage());
+			pex.printStackTrace();
+		} finally {
+			if( sc != null )
+			 sc.close(); //closes the scanner
+		}
 
     // show sorted films by length
-    for(Film film : films) System.out.println(film);
+    for(Film film : films) 
+			if(film != null)
+				System.out.println(film);
 
+		Scanner in = null;
+		try {
+			in = new Scanner(System.in);
+			System.out.print("Whats your goal length: ");
+			float goal = in.nextFloat();
 
-    Scanner in = new Scanner(System.in);
-    System.out.print("Whats your goal length: ");
-    float goal = in.nextFloat();
-    // binary search to find films
-
-    for(Film film : binarySearching(films, goal) ) {
-      System.out.println(film);
-    }
+			// binary search to find films
+			for(Film film : binarySearching(films, goal) ) {
+				System.out.println(film);
+			}
+		} catch (InputMismatchException imex ){
+			System.out.println(imex.getMessage());
+		}finally{
+			if( in != null )
+			 in.close();
+		}
     
-    in.close();
 	}
 
-  public static void timeToSort(Film []array, int size){
+  public static void timeToSort(Film []array, int size) throws RuntimeException{
     System.out.println("-----------------------------------");
 
     long start = System.currentTimeMillis(); // 0.02
@@ -61,7 +112,7 @@ public class ReadFilmData {
     System.out.println( size + " inputs || total elapsed time: " + result + "ms");
   }
 
-	public static void mergeSort(Film[] array, int size){
+	public static void mergeSort(Film[] array, int size) throws RuntimeException {
 
 		if(size < 2) return;
 
@@ -81,7 +132,7 @@ public class ReadFilmData {
 		merge(array, l, r, mid, size-mid);
   }
 
-  public static void merge(Film[] merged, Film[] left, Film[] right, int leftSize, int rightSize){
+  public static void merge(Film[] merged, Film[] left, Film[] right, int leftSize, int rightSize) throws RuntimeException{
 
       int i=0, j=0, k=0;
       // i é a posição do array left
@@ -113,7 +164,7 @@ public class ReadFilmData {
   }
 
   
-  public static Film[] binarySearching( Film[] array , float goal ){
+  public static Film[] binarySearching( Film[] array , float goal ) throws RuntimeException{
     
     int size = array.length;
     int start = 0;
@@ -243,11 +294,13 @@ class Film implements Comparable<Object>{
 		/*
 		Edit this section so it compares the appropriate
 		column you wish to sort by
-		*/
-
+		*/		
 		Film flm = (Film)obj;
-
-		if( length == flm.getLength() ) return 0;
+		
+		if (this == null && obj == null ) return 0;
+		else if( flm == null ) return 1;
+		else if( this == null ) return -1;
+		else if( length == flm.getLength() ) return 0;
 		else if( length > flm.getLength() ) return 1;
 		else return -1;
 	}
